36ea9977e5722240182a5ff2f51fb7826a988c38
Kersten Breuer <kersten-breuer@outlook.com>
2023-11-16 08:54:05 +0100

    Update tests/test_load.py
    Co-authored-by: Christoph Zwerschke <cito@online.de>

--- tests/test_load.py
+++ tests/test_load.py
@@ -33,7 +33,7 @@ def test_load_schemapack_valid(path: Path):
 
 
 @pytest.mark.parametrize(
-    "name, path", INVALID_SCHEMAPACK_PATHS.items(), ids=INVALID_SCHEMAPACK_PATHS.keys()
+    "name, path", INVALID_SCHEMAPACK_PATHS.items(), ids=INVALID_SCHEMAPACK_PATHS
 )
 def test_load_schemapack_invalid(name: str, path: Path):
     """Test loading invalid schemapacks."""
