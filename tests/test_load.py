958498572a43b96c3e3c5031d9111a76c7c2ffe1
Kersten Breuer <kersten-breuer@outlook.com>
2023-11-16 08:53:01 +0100

    Update tests/test_load.py
    Co-authored-by: Christoph Zwerschke <cito@online.de>

--- tests/test_load.py
+++ tests/test_load.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test dummy."""
+"""Test schemapack loading."""
 
 from pathlib import Path
 
