ea0f8fedbf262e23c614b4970bad23c828bbc32b
Kersten Henrik Breuer <kersten-breuer@outlook.com>
2024-03-20 15:17:59 +0100

    Update src/schemapack/_internals/dump.py
    Co-authored-by: Leon Kuchenbecker <leon.kuchenbecker@uni-tuebingen.de>

--- src/schemapack/_internals/dump.py
+++ src/schemapack/_internals/dump.py
@@ -26,6 +26,7 @@ from schemapack._internals.spec.schemapack import SchemaPack
 
 yaml = ruamel.yaml.YAML(typ="safe")
 yaml.indent(mapping=2, sequence=4, offset=2)
+yaml.default_flow_style = False
 
 
 def get_content_schema_path(*, class_name: str, content_schema_dir: Path) -> Path:
