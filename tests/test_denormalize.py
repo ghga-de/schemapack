cd7861b881a0ca8ee31603d785e9f33c75823e1e
sbilge <bilgesurun@gmail.com>
2025-03-11 13:46:55 +0100

    Update tests/test_denormalize.py
    Co-authored-by: Thomas Zajac <thomas-jakob.zajac@uni-tuebingen.de>

--- tests/test_denormalize.py
+++ tests/test_denormalize.py
@@ -41,8 +41,8 @@ def test_denormalize_deep_embedding(name: str, expected_denormalized_path: Path)
     schemapack_name = name.split(".")[0]
     schemapack = load_schemapack(VALID_SCHEMAPACK_PATHS[schemapack_name])
     datapack = load_datapack(VALID_DATAPACK_PATHS[name])
-    expected_denormalizated = read_json_or_yaml_mapping(expected_denormalizated_path)
-    denormalizated = denormalize(datapack=datapack, schemapack=schemapack)
+    expected_denormalized = read_json_or_yaml_mapping(expected_denormalized_path)
+    denormalized = denormalize(datapack=datapack, schemapack=schemapack)
 
     assert denormalizated == expected_denormalizated
 
