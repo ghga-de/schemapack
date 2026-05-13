5bc46915be64a8ad0b26bb9fda193dfe8ee01d83
sbilge <bilge.sueruen@uni-tuebingen.de>
2026-05-13 15:45:08 +0000

    removed the inline annotation.

--- src/schemapack/_internals/validation/plugins/globally_unique_ids.py
+++ src/schemapack/_internals/validation/plugins/globally_unique_ids.py
@@ -48,7 +48,7 @@ class GloballyUniqueIdsValidationPlugin(GlobalValidationPlugin):
         Raises:
             schemapack.exceptions.ValidationPluginError: If validation fails.
         """
-        id_to_classes: dict[str, list[str]] = defaultdict(list)
+        id_to_classes= defaultdict(list)
         for class_name, resources in datapack.resources.items():
             for resource_id in resources:
                 id_to_classes[resource_id].append(class_name)
